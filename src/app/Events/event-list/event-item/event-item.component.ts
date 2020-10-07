f3f734db5469eeec5137b7f65594f12705107a49
Andriy03092001 <cuanid236316@gmail.com>
2020-10-07 15:15:59 +0300

    add show list events

--- src/app/Events/event-list/event-item/event-item.component.ts
+++ src/app/Events/event-list/event-item/event-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Event } from '../../event.model';
 
 @Component({
@@ -9,12 +9,15 @@ import { Event } from '../../event.model';
 export class EventItemComponent implements OnInit {
 
   constructor() { }
+  // currentEvent = new Event("JS Event in Kiev", "...", "20/10/2020", "https://meline.lviv.ua/wp-content/uploads/2019/06/javascript-1231.png");
 
-  // title: string = "JS Event in Kiev";
-  // image_url: string = "https://meline.lviv.ua/wp-content/uploads/2019/06/javascript-1231.png";
-  // dateStart: string = "20/10/2020";
+  @Input() currentEvent: Event;
+  @Input() index: number;
+  isPriority: boolean = true;
 
-  currentEvent = new Event("JS Event in Kiev", "...", "20/10/2020", "https://meline.lviv.ua/wp-content/uploads/2019/06/javascript-1231.png");
+  setPriority() {
+    this.isPriority = !this.isPriority;
+  }
 
   ngOnInit() {
   }
--- src/app/Events/event-list/event-list.component.ts
+++ src/app/Events/event-list/event-list.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from '../event.model';
 
 @Component({
   selector: 'event-list',
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.css']
 })
+
 export class EventListComponent implements OnInit {
 
   constructor() { }
+
+  events: Event[] = [
+    new Event("Visit JS Event", "...", "20/12/2020", "https://meline.lviv.ua/wp-content/uploads/2019/06/javascript-1231.png"),
+    new Event("Course Work ITSTEP", "...", "15/10/2020", "https://i.ytimg.com/vi/0M-eCdRZ0lo/maxresdefault.jpg"),
+    new Event("Buy new laptop", "...", "12/12/2020", "https://c.s-microsoft.com/ru-ru/CMSImages/1920_Panel1_Hero_Laptop.jpg?version=090f3049-8032-1102-e65b-498904a634e1")
+  ];
 
   ngOnInit() {
   }
